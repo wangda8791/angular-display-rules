4256a95e229557ffdce6ba96130c4b9172432ca4
Wang Da <wangda8791@163.com>
2020-10-14 17:18:25 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RuleComponent } from './rule/rule.component';
+import { RuleGroupComponent } from './rule-group/rule-group.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RuleComponent,
+    RuleGroupComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
