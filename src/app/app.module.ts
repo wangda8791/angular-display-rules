3fdbafb056852f6a61baba3d03a975bd17299973
Wang Da <wangda8791@163.com>
2020-10-21 07:25:38 -0400

    Add header of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { RuleComponent } from './rule/rule.component';
 import { RuleGroupComponent } from './rule-group/rule-group.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     RuleComponent,
     RuleGroupComponent,
   ],
